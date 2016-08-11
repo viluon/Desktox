c7f93889d6954eb77e0f5bbc7c7f149bbadff82d
Andrew Kvapil <viluon@espiv.net>
2016-08-11 10:39:11 +0200

    Update Tests

--- test_buffer.lua
+++ test_buffer.lua
@@ -1,8 +1,9 @@
 
 local buffer = dofile "buffer.lua"
 
-local b = buffer.new( 1, 1, 5, 3 )
+local b1 = buffer.new( 1, 1, 10, 10 )
+local b2 = buffer.new( 2, 1, 4, 2, b1, 0 )
 
-b:render_to_window( term.current() )
- :resize( 10, 7, colours.green )
- :render_to_window( term.current() )
+b2:resize( 5, 4, colours.blue ):render( nil, nil, nil, 1, 2 )
+
+b1:render_to_window( term.current() )
