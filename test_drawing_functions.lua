07532dc16c18493bf3d6271bb8c82e0be51a6d61
Andrew Kvapil <viluon@espiv.net>
2016-08-13 16:04:09 +0200

    Update Test

--- test_drawing_functions.lua
+++ test_drawing_functions.lua
@@ -8,16 +8,14 @@ local terminal = term.current()
 local main_buf = buffer.new( 0, 0, w, h )
 local edit_buf = buffer.new( 0, 0, math.floor( w / 2 ), math.floor( h / 2 ), main_buf )
 
-edit_buf:draw_filled_rectangle( 2, 2, 8, 5, colours.green ):map(
-	function( self, x, y, pixel )
-		return {
-			math.random() > 0.5 and pixel[ 1 ] or -1;
-			pixel[ 2 ];
-			pixel[ 3 ];
-		}
-	end
-)()
 
+main_buf
+:draw_circle( 20, 9, 7, colours.green )
+--[[
+:draw_rectangle_using_points( 1, 1, 20, 9, 2, colours.green )
+:draw_rectangle_using_points( 30, 5, 36, 11, 1, colours.orange )
+:draw_rectangle_using_points( 4, 11, 10, 17, 3, colours.blue )
+--]]
 
 main_buf( terminal )
 
