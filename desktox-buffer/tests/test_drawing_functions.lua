e097cf3ab1334c798269b092589225772033c5c1
Andrew Kvapil <viluon@espiv.net>
2016-08-15 13:16:04 +0200

    Update Test

--- desktox-buffer/tests/test_drawing_functions.lua
+++ desktox-buffer/tests/test_drawing_functions.lua
@@ -10,7 +10,9 @@ local edit_buf = buffer.new( 0, 0, math.floor( w / 2 ), math.floor( h / 2 ), mai
 
 
 main_buf
-:draw_circle( 20, 9, 7, colours.green )
+:draw_circle( 22, 9, 1, colours.green )
+:draw_circle( 10, 7, 8, colours.blue )
+:draw_circle( 40, 12, 6, colours.red )
 --[[
 :draw_rectangle_using_points( 1, 1, 20, 9, 2, colours.green )
 :draw_rectangle_using_points( 30, 5, 36, 11, 1, colours.orange )
